41438e4fcdd87a98a759e0c9bd1bb5b7131f17b8
xzp <xingzepeng@geo-compass.com>
2022-06-06 18:01:21 +0800

    feat: history

--- 2.router/src/history/createBrowserHistory.js
+++ 2.router/src/history/createBrowserHistory.js
@@ -1,3 +1,22 @@
-function createBrowserHistory() {}
+function createBrowserHistory() {
+  const globalHistory = window.history;
+
+  let state;
+  let listeners = [];
+
+  function listen(listener) {
+    listeners.push(listener);
+
+    return () => {
+      listeners = listeners.filter((item) => item !== listener);
+    };
+  }
+
+  const history = {
+    listen,
+  };
+
+  return history;
+}
 
 export default createBrowserHistory;
